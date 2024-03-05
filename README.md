9e921d86ee81ebc4567f5ee54e0493858ee0d501
ALTR <chuan.khuna@gmail.com>
2024-03-05 19:39:01 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Astro Resume
+
+based on [AREA44's astro-shadcn-ui-template](https://github.com/AREA44/astro-shadcn-ui-template)
+
+```bash
+docker compose up -d
+```
---
+++ src/components/bento/beautiful-bento.tsx
@@ -0,0 +1,112 @@
+import { cn } from '@/utils/cn'
+import React from 'react'
+import { motion } from "framer-motion";
+import { BentoGrid, BentoGridItem } from '@/components/aceternity/bento-grid'
+import {
+  IconArrowWaveRightUp,
+  IconBoxAlignRightFilled,
+  IconBoxAlignTopLeft,
+  IconClipboardCopy,
+  IconFileBroken,
+  IconSignature,
+  IconTableColumn,
+} from '@tabler/icons-react'
+
+export function BentoDemo() {
+  return (
+    <BentoGrid className="mx-auto max-w-4xl">
+      {items.map((item, i) => (
+        <BentoGridItem
+          key={i}
+          title={item.title}
+          description={item.description}
+          header={item.header}
+          icon={item.icon}
+          className={i === 3 || i === 6 ? 'md:col-span-2' : ''}
+        />
+      ))}
+    </BentoGrid>
+  )
+}
+
+const SkeletonGradient = () => {
+  const variants = {
+    initial: {
+      backgroundPosition: '0 50%',
+    },
+    animate: {
+      backgroundPosition: ['0, 50%', '100% 50%', '0 50%'],
+    },
+  }
+  return (
+    <motion.div
+      initial="initial"
+      animate="animate"
+      variants={variants}
+      transition={{
+        duration: 5,
+        repeat: Infinity,
+        repeatType: 'reverse',
+      }}
+      className="dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex h-full min-h-[6rem] w-full flex-1 flex-col space-y-2 rounded-lg"
+      style={{
+        background:
+          'linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)',
+        backgroundSize: '400% 400%',
+      }}
+    >
+      <motion.div className="h-full w-full rounded-lg"></motion.div>
+    </motion.div>
+  )
+}
+
+// bento card highlight
+const Skeleton = () => (
+  <div className="flex h-full min-h-[6rem] w-full flex-1 rounded-xl bg-gradient-to-br from-neutral-200 to-neutral-100 dark:from-neutral-900 dark:to-neutral-800"></div>
+)
+
+const items = [
+  {
+    title: 'The Dawn of Innovation',
+    description: 'Explore the birth of groundbreaking ideas and inventions.',
+    header: <SkeletonGradient />,
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: 'The Digital Revolution',
+    description: 'Dive into the transformative power of technology.',
+    header: <Skeleton />,
+    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: 'The Art of Design',
+    description: 'Discover the beauty of thoughtful and functional design.',
+    header: <Skeleton />,
+    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: 'The Power of Communication',
+    description:
+      'Understand the impact of effective communication in our lives.',
+    header: <SkeletonGradient />,
+    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: 'The Pursuit of Knowledge',
+    description: 'Join the quest for understanding and enlightenment.',
+    header: <Skeleton />,
+    icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: 'The Joy of Creation',
+    description: 'Experience the thrill of bringing ideas to life.',
+    header: <Skeleton />,
+    icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: 'The Spirit of Adventure',
+    description: 'Embark on exciting journeys and thrilling discoveries.',
+    header: <SkeletonGradient />,
+    icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+  },
+]
