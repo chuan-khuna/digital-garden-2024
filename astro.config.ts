834bba86b970ac410d8d72eef43941e7b1bed04c
ALTR <chuan.khuna@gmail.com>
2024-03-06 19:55:44 +0700

    add mdx support

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI
-    ? 'https://github.com/chuan-khuna'
-    : 'http://localhost:4321',
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
-})
+  site: process.env.CI ? 'https://github.com/chuan-khuna' : 'http://localhost:4321',
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), mdx()]
+});
