8f0c614dec17571129619ec69c92404ebc5f2ed6
ALTR <chuan.khuna@gmail.com>
2024-03-07 21:30:49 +0700

    update collection definitions

---
+++ src/content/collection-definitions/project.ts
@@ -0,0 +1,10 @@
+import { z, defineCollection } from "astro:content";
+
+export const projectsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    date: z.string().optional(),
+  }),
+});
