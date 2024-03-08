93325e16f9a7995e7bb7cbb480b8dc055f81f0be
ALTR <chuan.khuna@gmail.com>
2024-03-08 22:45:03 +0700

    add back reference card

--- src/components/aceternity/bento-grid.tsx
+++ src/components/aceternity/bento-grid.tsx
@@ -1,23 +1,23 @@
-import { cn } from "@/utils/cn";
+import { cn } from '@/utils/cn'
 
 export const BentoGrid = ({
   className,
   children,
 }: {
-  className?: string;
-  children?: React.ReactNode;
+  className?: string
+  children?: React.ReactNode
 }) => {
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
-        className
+        'mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-3 ',
+        className,
       )}
     >
       {children}
     </div>
-  );
-};
+  )
+}
 
 // bento card
 export const BentoGridItem = ({
@@ -27,29 +27,29 @@ export const BentoGridItem = ({
   header,
   icon,
 }: {
-  className?: string;
-  title?: string | React.ReactNode;
-  description?: string | React.ReactNode;
-  header?: React.ReactNode;
-  icon?: React.ReactNode;
+  className?: string
+  title?: string | React.ReactNode
+  description?: string | React.ReactNode
+  header?: React.ReactNode
+  icon?: React.ReactNode
 }) => {
   return (
     <div
       className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border justify-between flex flex-col space-y-4",
-        className
+        'group/bento row-span-1 flex flex-col justify-between space-y-4 rounded-xl border bg-white p-4 shadow-input transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none',
+        className,
       )}
     >
       {header}
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
+      <div className="transition duration-200 group-hover/bento:translate-x-2">
         {icon}
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+        <div className="mb-2 mt-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
           {title}
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
           {description}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
---
+++ src/components/back_ref/backref.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {
+  CardImageGradient,
+  CardImageDot,
+} from '@/components/placeholder/card-image'
+
+const BackReferenceCard = ({ title, description, date }) => {
+  return (
+    <div className="group/bento row-span-1 flex flex-col justify-between space-y-4 rounded-xl border bg-white p-4 shadow-input transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none">
+      <CardImageDot />
+      <div className="transition duration-200 group-hover/bento:translate-x-2">
+        <div className="mb-2 mt-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
+          {title}
+        </div>
+        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
+          {description}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export const BackReferenceSection = ({ incomingReferences }) => {
+  console.log(incomingReferences)
+  return (
+    <div className="mx-auto max-w-4xl">
+      Referenced in:
+      {incomingReferences.map((item, idx) => (
+        <BackReferenceCard title={item.data.title} description={item.slug} />
+      ))}
+    </div>
+  )
+}
