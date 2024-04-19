043e7d8c06f607aaa55da710f8ddd6b391b379f9
ALTR <chuan.khuna@gmail.com>
2024-04-19 14:18:35 +0700

    update post card colour

--- src/components/post/post-card.tsx
+++ src/components/post/post-card.tsx
@@ -4,13 +4,13 @@ import { CardImageDot } from '@/components/placeholder/card-image'
 export const PostCard = ({ rootURL, slug, title, description, date }) => {
   return (
     <a href={`/${rootURL}/${slug}`}>
-      <div className="group/backref row-span-1 flex flex-col justify-between space-y-4 rounded-xl border bg-white p-4 shadow-input transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none">
+      <div className="group/backref row-span-1 flex flex-col justify-between space-y-4 rounded-xl border bg-card p-4 shadow-input transition duration-200 hover:shadow-xl">
         <CardImageDot />
         <div className="transition duration-200 group-hover/backref:translate-x-2">
-          <div className="mb-2 mt-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
+          <div className="mb-2 mt-2 font-sans font-bold text-foreground">
             {title}
           </div>
-          <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
+          <div className="font-sans text-xs font-normal text-foreground">
             {description}
           </div>
         </div>
