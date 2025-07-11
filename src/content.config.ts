63526b0ceab71bda939266b1a9a9eee87216363c
ALTR <chuan.khuna@gmail.com>
2025-07-11 22:14:56 +0700

    try to create resume content collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,8 @@
-import { postsCollection } from "@/content/collection-definitions/post";
+import { postsCollection } from '@/content/collection-definitions/post'
+
+import { resumeSkillsCollection } from '@/content/collection-definitions/resume'
 
 export const collections = {
   posts: postsCollection,
+  resumeSkills: resumeSkillsCollection,
 }
--- src/content/collection-definitions/post.ts
+++ src/content/collection-definitions/post.ts
@@ -1,9 +1,8 @@
 import { z, defineCollection } from 'astro:content'
-import { glob } from "astro/loaders";
-
+import { glob } from 'astro/loaders'
 
 export const postsCollection = defineCollection({
-  loader: glob({ base: "./src/content/posts", pattern: "**/*.{md,mdx}" }),
+  loader: glob({ base: '@/content/posts', pattern: '**/*.{md,mdx}' }),
   // type: 'content',
   schema: z.object({
     title: z.string(),
