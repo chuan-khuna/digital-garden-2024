a4ce28df7af0151bf16827cae98b1ff09d743e21
ALTR <chuan.khuna@gmail.com>
2025-07-30 17:03:25 +0700

    feat: add resume skills collection and integrate into resume page

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,10 @@
 import { postsCollection } from '@/content/collection-definitions/post'
 
+import { resumeSkillsCollection } from '@/content/collection-definitions/skills'
+
+
+
 export const collections = {
   posts: postsCollection,
+  resumeSkills: resumeSkillsCollection
 }
