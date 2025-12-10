322985f8b766911f53bdfb389742839b9d9c7792
ALTR <chuan.khuna@gmail.com>
2025-12-10 20:38:19 +0700

    update content collection for resume

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,11 +4,15 @@ import { navItemsCollection } from '@/content/collection-definitions/nav'
 import {
   resumeSkillsCollection,
   resumeProjectsCollection,
+  resumeExperiencesCollection,
+  resumeEducationsCollection,
 } from '@/content/collection-definitions/resume'
 
 export const collections = {
   posts: postsCollection,
   resumeSkills: resumeSkillsCollection,
   resumeProjects: resumeProjectsCollection,
+  resumeExperiences: resumeExperiencesCollection,
+  resumeEducations: resumeEducationsCollection,
   navItems: navItemsCollection,
 }
--- src/content/collection-definitions/resume.ts
+++ src/content/collection-definitions/resume.ts
@@ -31,3 +31,45 @@ export const resumeProjectsCollection = defineCollection({
       }),
   }),
 })
+
+export const resumeExperiencesCollection = defineCollection({
+  loader: file('src/content/resume/experiences.json'),
+  schema: z.object({
+    jobTitle: z.string(),
+    company: z.string(),
+    time: z.string(),
+    details: z.array(z.string()),
+    visibility: z
+      .object({
+        web: z.boolean(),
+        resume_print: z.boolean(),
+        cv_print: z.boolean(),
+      })
+      .default({
+        web: true,
+        resume_print: true,
+        cv_print: true,
+      }),
+  }),
+})
+
+export const resumeEducationsCollection = defineCollection({
+  loader: file('src/content/resume/educations.json'),
+  schema: z.object({
+    degree: z.string(),
+    institution: z.string(),
+    time: z.string(),
+    details: z.array(z.string()),
+    visibility: z
+      .object({
+        web: z.boolean(),
+        resume_print: z.boolean(),
+        cv_print: z.boolean(),
+      })
+      .default({
+        web: true,
+        resume_print: true,
+        cv_print: true,
+      }),
+  }),
+})
